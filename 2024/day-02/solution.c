90e188c5624e957100cc040406de379930df9c39
Elias20102 <Elias.ralph.merz@gmail.com>
2024-12-04 01:23:28 +0100

    :(

--- 2024/day-02/solution.c
+++ 2024/day-02/solution.c
@@ -3,51 +3,58 @@
 #include<stdlib.h>
 #include<math.h>
 
-int check(int val, int lastVal, int* direction){
-	
-			if( abs(val - lastVal) > 3 || abs(val - lastVal) == 0 || (*direction != 0 && (val - lastVal > 0) - (val - lastVal < 0) != *direction)){
-					return 0;
-			}
-			if(*direction == 0) *direction = (val - lastVal > 0) - (val - lastVal < 0);
-			return 1;
-			
-}
-
-int attempt(char *line, int lastVal,int carry,int safe, int airbag,int direction ,int skip, int index) {
+int attempt(char *line, int prev,int carry,int safe, int airbag,int direction,int skip, int index) {
 		char* token;
 		int val;
-		int resultA;
-		int resultB;
-		if(skip == 1){
-			val = carry;
-		}
-		else{
+		int result = 0;
+		if(skip == 0){
 			token = strtok_r(line," \n",&line);
 			
 			if (token == NULL) {
-				return safe + airbag;
+				return safe+airbag;
 			}
 			val = atoi(token);
 		}
-		if(check(val,lastVal,&direction) == 0){
-			safe = 0;
-			if(airbag == 0 && safe == 0) {
+		else{
+			val = carry;
+			carry = -1;
+		}
+		
+		if( direction == 0){
+			if(carry == -1){
+				direction = (val - prev > 0) - (val - prev < 0);
+			}
+			else{
+				direction = (prev - carry > 0) -  (prev - carry < 0);
+			}
+		}
+			
+		if( abs(val - prev) > 3 || abs(val - prev) == 0 || (direction != 0 && (val - prev > 0) - (val - prev < 0) != direction)){
+			if(airbag == 0){
 				return 0;
 			}
-			else if(safe == 0 && airbag == 1){
-				airbag = 0;
-				safe = 1;
-				if(carry != -1){ 
-				resultA = attempt(strdup(line),lastVal,carry,safe,airbag,direction,0,index+1);
-				resultB = attempt(strdup(line),carry,val,safe,airbag,direction,1,index+1);
-				if(resultA < resultB) return resultB;
-				else return resultA;
-				}
+			airbag = 0;
+			if(carry == -1 || (carry != -1 && !( abs(carry - val) > 3 || abs(carry - val) == 0) && index == 2 )){
+				result = attempt(strdup(line),val,carry,safe,airbag,0,0,index+1);
+				if(result == 1 ) return result;
 			}
+			if(carry != -1 && !( abs(carry - val) > 3 || abs(carry - val) == 0) && index != 2){
+					result = attempt(strdup(line),val,carry,safe,airbag,direction,0,index+1);
+				if(result == 1 ) return result;
+				
+				
+			}
+			if(index == 2){
+				result = attempt(strdup(line),prev,val,safe,airbag,0,1,index);
+				if(result == 1 ) return result;
+			}
+			
+			result = attempt(strdup(line),prev,carry,safe,airbag,direction,0,index+1);
+			return result;
 			
 		}
 		
-		return attempt(line,val,lastVal,safe,airbag,direction,0,index+1);
+		return attempt(strdup(line),val,prev,safe,airbag,direction,0,index+1);
 	}
 
 int main(){
@@ -56,19 +63,22 @@ int main(){
 	
 	char line[64];
 	char* token;
-	
 	int result;
 	
 	while(fgets(line, sizeof(line), stdin)){
-		
+	
 		char *line_copy = strdup(line);
 		token = strtok_r(line_copy," \n",&line_copy);
 		result = attempt(line_copy,atoi(token),-1,1,1,0,0,1);
-		if(result == 2) part1++;
-		else if(result > 0) part2++;
-	}
-	part2+=part1;
 		
+		if(result == 2){
+			part1++;
+			part2++;
+		}
+		else if(result == 1){
+			part2++;
+		}
+	}
 	
 	printf("%d\n",part1);
 	printf("%d\n",part2);
